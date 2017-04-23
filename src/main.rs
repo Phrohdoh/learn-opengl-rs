1488f7733b2c80de34b493fc6033a3e8f32fa5a1
Taryn Hill <taryn@phrohdoh.com>
2017-04-22 21:05:09 -0500

    Implement Gouraud shading

--- src/main.rs
+++ src/main.rs
@@ -13,27 +13,35 @@ fn main() {
         .expect("Failed to build glium window");
 
     let vs = r#"
-        #version 140
+        #version 330
 
         in vec3 position;
         in vec3 normal;
+
+        out vec3 v_normal;
 
         uniform mat4 matrix;
 
         // Called once for each vertex in our geometry
         void main() {
+            v_normal = transpose(inverse(mat3(matrix))) * normal;
             gl_Position = matrix * vec4(position, 1.0);
         }
     "#;
 
     let fs = r#"
-        #version 140
+        #version 330
 
+        in vec3 v_normal;
         out vec4 color;
+        uniform vec3 u_light;
 
         // Called once per pixel
         void main() {
-            color = vec4(1.0, 0.0, 0.0, 1.0);
+            float brightness = dot(normalize(v_normal), normalize(u_light));
+            vec3 dark_color = vec3(0.6, 0.0, 0.0);
+            vec3 regular_color = vec3(1.0, 0.0, 0.0);
+            color = vec4(mix(dark_color, regular_color, brightness), 1.0);
         }
     "#;
 
@@ -41,6 +49,8 @@ fn main() {
     let normals = glium::VertexBuffer::new(&display, &teapot::NORMALS).unwrap();
     let indices = glium::IndexBuffer::new(&display, glium::index::PrimitiveType::TrianglesList, &teapot::INDICES).unwrap();
     let program = glium::Program::from_source(&display, vs, fs, None).expect("Failed to link program");
+
+    let light = [-1.0, 0.4, 0.8f32];
 
     loop {
         let mut target = display.draw();
@@ -53,6 +63,7 @@ fn main() {
                 [0.0, 0.0, 0.01, 0.0],
                 [0.0, 0.0, 0.0, 1.0f32]
             ],
+            u_light: light,
         };
 
         target.draw((&positions, &normals), &indices, &program, &uniforms, &Default::default())
