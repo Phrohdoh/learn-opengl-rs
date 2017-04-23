d5551e852a0029f34d42e8ee7a79c684a1fb3355
Taryn Hill <taryn@phrohdoh.com>
2017-04-22 21:48:45 -0500

    Implement the view matrix

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,40 @@ use glium::Surface;
 
 mod teapot;
 
+fn calc_view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
+    let f = {
+        let f = direction;
+        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
+        let len = len.sqrt();
+        [f[0] / len, f[1] / len, f[2] / len]
+    };
+
+    let s = [up[1] * f[2] - up[2] * f[1],
+             up[2] * f[0] - up[0] * f[2],
+             up[0] * f[1] - up[1] * f[0]];
+
+    let s_norm = {
+        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
+        let len = len.sqrt();
+        [s[0] / len, s[1] / len, s[2] / len]
+    };
+
+    let u = [f[1] * s_norm[2] - f[2] * s_norm[1],
+             f[2] * s_norm[0] - f[0] * s_norm[2],
+             f[0] * s_norm[1] - f[1] * s_norm[0]];
+
+    let p = [-position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
+             -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
+             -position[0] * f[0] - position[1] * f[1] - position[2] * f[2]];
+
+    [
+        [s[0], u[0], f[0], 0.0],
+        [s[1], u[1], f[1], 0.0],
+        [s[2], u[2], f[2], 0.0],
+        [p[0], p[1], p[2], 1.0],
+    ]
+}
+
 fn main() {
     use glium::DisplayBuild;
     let display = glium::glutin::WindowBuilder::new()
@@ -22,12 +56,14 @@ fn main() {
         out vec3 v_normal;
 
         uniform mat4 perspective;
-        uniform mat4 matrix;
+        uniform mat4 view_matrix;
+        uniform mat4 model_matrix;
 
         // Called once for each vertex in our geometry
         void main() {
-            v_normal = transpose(inverse(mat3(matrix))) * normal;
-            gl_Position = perspective * matrix * vec4(position, 1.0);
+            mat4 mv = view_matrix * model_matrix;
+            v_normal = transpose(inverse(mat3(mv))) * normal;
+            gl_Position = perspective * mv * vec4(position, 1.0);
         }
     "#;
 
@@ -68,12 +104,14 @@ fn main() {
         ..Default::default()
     };
 
-    let matrix = [
+    let model_matrix = [
         [0.01, 0.0, 0.0, 0.0],
         [0.0, 0.01, 0.0, 0.0],
         [0.0, 0.0, 0.01, 0.0],
         [0.0, 0.0, 2.0, 1.0f32]
     ];
+
+    let view_matrix = calc_view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]);
 
     let light = [-1.0, 0.4, 0.8f32];
 
@@ -103,7 +141,8 @@ fn main() {
                   &indices,
                   &program,
                   &uniform! {
-                    matrix: matrix,
+                    model_matrix: model_matrix,
+                    view_matrix: view_matrix,
                     u_light: light,
                     perspective: perspective,
                   },
